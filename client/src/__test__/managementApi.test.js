c1bb7d523ea2e60129438272847265db8e9c1a75
Casey Wittrock <CwittrockC@yorksolutions.com>
2024-12-28 02:17:00 -0600

    create api for sending requests to the backend and passing tests. updated gitignore

---
+++ client/src/__test__/managementApi.test.js
@@ -0,0 +1,290 @@
+import { describe, test, expect, afterEach, jest } from "@jest/globals";
+
+// const BASE_URL = import.meta.env.VITE_APP_BASE_URL;
+
+jest.mock("axios");
+import axios from "axios";
+import ManagementApi from "../managementApi";
+
+describe("ManagementApi", () => {
+  afterEach(() => {
+    jest.clearAllMocks();
+    ManagementApi.token = undefined;
+  });
+
+  // setup tests
+  test("should set the token", async () => {
+    ManagementApi.setToken("token");
+    expect(ManagementApi.token).toBe("token");
+  });
+
+  // request tests
+
+  test("should send a request and get a response", async () => {
+    const mockData = { mockData: "mock data" };
+    axios.mockResolvedValue({ data: mockData });
+    const result = await ManagementApi.request("/path", "get");
+
+    expect(result).toEqual(mockData);
+    expect(axios).toHaveBeenCalledTimes(1);
+    expect(axios).toHaveBeenCalledWith({
+      method: "get",
+      url: "/api/path",
+      data: {},
+      headers: {},
+      withCredentials: true,
+    });
+  });
+
+  test("request should catch an error", async () => {
+    axios.mockRejectedValue({
+      response: { data: { error: { message: "error" } } },
+    });
+
+    try {
+      await ManagementApi.request("/path", "get");
+    } catch (error) {
+      expect(error).toEqual(["error"]);
+    }
+  });
+
+  // patient tests
+
+  test("should create a new patient", async () => {
+    const mockData = { mockData: "mock data" };
+    axios.mockResolvedValue({ data: mockData });
+    ManagementApi.setToken("token");
+    const result = await ManagementApi.createPatient({ name: "name" });
+
+    expect(result).toEqual(mockData);
+    expect(axios).toHaveBeenCalledTimes(1);
+    expect(axios).toHaveBeenCalledWith({
+      method: "post",
+      url: "/api/patients",
+      data: { name: "name" },
+      headers: { "X-XSRF-TOKEN": "token" },
+      withCredentials: true,
+    });
+  });
+
+  test("should get patient by id", async () => {
+    const mockData = { mockData: "mock data" };
+    axios.mockResolvedValue({ data: mockData });
+    ManagementApi.setToken("token");
+    const result = await ManagementApi.getPatient("abcd1234");
+
+    expect(result).toEqual(mockData);
+    expect(axios).toHaveBeenCalledTimes(1);
+    expect(axios).toHaveBeenCalledWith({
+      method: "get",
+      url: "/api/patients/abcd1234",
+      data: {},
+      headers: { "X-XSRF-TOKEN": "token" },
+      withCredentials: true,
+    });
+  });
+
+  test("should get page of patients with defaults no filter", async () => {
+    const mockData = { mockData: "mock data" };
+    axios.mockResolvedValue({ data: mockData });
+    ManagementApi.setToken("token");
+    const result = await ManagementApi.getPagePatients();
+
+    expect(result).toEqual(mockData);
+    expect(axios).toHaveBeenCalledTimes(1);
+    expect(axios).toHaveBeenCalledWith({
+      method: "get",
+      url: "/api/patients?page=1&limit=10",
+      data: {},
+      headers: { "X-XSRF-TOKEN": "token" },
+      withCredentials: true,
+    });
+  });
+
+  test("should get page of patients with filter", async () => {
+    const mockData = { mockData: "mock data" };
+    axios.mockResolvedValue({ data: mockData });
+    ManagementApi.setToken("token");
+    const result = await ManagementApi.getPagePatients(
+      2,
+      20,
+      "filter",
+      "filterValue"
+    );
+
+    expect(result).toEqual(mockData);
+    expect(axios).toHaveBeenCalledTimes(1);
+    expect(axios).toHaveBeenCalledWith({
+      method: "get",
+      url: "/api/patients?page=2&limit=20&filter=filter&filterValue=filterValue",
+      data: {},
+      headers: { "X-XSRF-TOKEN": "token" },
+      withCredentials: true,
+    });
+  });
+
+  test("should update patient", async () => {
+    const mockData = { mockData: "mock data" };
+    axios.mockResolvedValue({ data: mockData });
+    ManagementApi.setToken("token");
+    const result = await ManagementApi.updatePatient({
+      name: "name",
+    });
+
+    expect(result).toEqual(mockData);
+    expect(axios).toHaveBeenCalledTimes(1);
+    expect(axios).toHaveBeenCalledWith({
+      method: "put",
+      url: "/api/patients",
+      data: { name: "name" },
+      headers: { "X-XSRF-TOKEN": "token" },
+      withCredentials: true,
+    });
+  });
+
+  test("should delete patient by id", async () => {
+    const mockData = { mockData: "mock data" };
+    axios.mockResolvedValue({ data: mockData });
+    ManagementApi.setToken("token");
+    const result = await ManagementApi.deletePatient("abcd1234");
+
+    expect(result).toEqual(mockData);
+    expect(axios).toHaveBeenCalledTimes(1);
+    expect(axios).toHaveBeenCalledWith({
+      method: "delete",
+      url: "/api/patients/abcd1234",
+      data: {},
+      headers: { "X-XSRF-TOKEN": "token" },
+      withCredentials: true,
+    });
+  });
+
+  // prescription tests
+
+  test("should create a new prescription", async () => {
+    const mockData = { mockData: "mock data" };
+    axios.mockResolvedValue({ data: mockData });
+    ManagementApi.setToken("token");
+    const result = await ManagementApi.createPrescription({ name: "name" });
+
+    expect(result).toEqual(mockData);
+    expect(axios).toHaveBeenCalledTimes(1);
+    expect(axios).toHaveBeenCalledWith({
+      method: "post",
+      url: "/api/prescriptions",
+      data: { name: "name" },
+      headers: { "X-XSRF-TOKEN": "token" },
+      withCredentials: true,
+    });
+  });
+
+  test("should cancel a prescription", async () => {
+    const mockData = { mockData: "mock data" };
+    axios.mockResolvedValue({ data: mockData });
+    ManagementApi.setToken("token");
+    const result = await ManagementApi.cancelPrescription(3);
+
+    expect(result).toEqual(mockData);
+    expect(axios).toHaveBeenCalledTimes(1);
+    expect(axios).toHaveBeenCalledWith({
+      method: "patch",
+      url: "/api/prescriptions/3/cancel",
+      data: {},
+      headers: { "X-XSRF-TOKEN": "token" },
+      withCredentials: true,
+    });
+  });
+
+  test("should get page of prescriptions with defaults no filter", async () => {
+    const mockData = { mockData: "mock data" };
+    axios.mockResolvedValue({ data: mockData });
+    ManagementApi.setToken("token");
+    const result = await ManagementApi.getPagePrescriptions();
+
+    expect(result).toEqual(mockData);
+    expect(axios).toHaveBeenCalledTimes(1);
+    expect(axios).toHaveBeenCalledWith({
+      method: "get",
+      url: "/api/prescriptions?page=1&limit=10",
+      data: {},
+      headers: { "X-XSRF-TOKEN": "token" },
+      withCredentials: true,
+    });
+  });
+
+  test("should get page of prescriptions with filter", async () => {
+    const mockData = { mockData: "mock data" };
+    axios.mockResolvedValue({ data: mockData });
+    ManagementApi.setToken("token");
+    const result = await ManagementApi.getPagePrescriptions(
+      2,
+      20,
+      "filter",
+      "filterValue"
+    );
+
+    expect(result).toEqual(mockData);
+    expect(axios).toHaveBeenCalledTimes(1);
+    expect(axios).toHaveBeenCalledWith({
+      method: "get",
+      url: "/api/prescriptions?page=2&limit=20&filter=filter&filterValue=filterValue",
+      data: {},
+      headers: { "X-XSRF-TOKEN": "token" },
+      withCredentials: true,
+    });
+  });
+
+  test("should get prescription by id", async () => {
+    const mockData = { mockData: "mock data" };
+    axios.mockResolvedValue({ data: mockData });
+    ManagementApi.setToken("token");
+    const result = await ManagementApi.getPrescription(3);
+
+    expect(result).toEqual(mockData);
+    expect(axios).toHaveBeenCalledTimes(1);
+    expect(axios).toHaveBeenCalledWith({
+      method: "get",
+      url: "/api/prescriptions/3",
+      data: {},
+      headers: { "X-XSRF-TOKEN": "token" },
+      withCredentials: true,
+    });
+  });
+
+  // authentication tests
+
+  test("should get doctor user data from token", async () => {
+    const mockData = { mockData: "mock data" };
+    axios.mockResolvedValue({ data: mockData });
+    ManagementApi.setToken("token");
+    const result = await ManagementApi.authenticateDoctorUser();
+
+    expect(result).toEqual(mockData);
+    expect(axios).toHaveBeenCalledTimes(1);
+    expect(axios).toHaveBeenCalledWith({
+      method: "post",
+      url: "/api/auth",
+      data: {},
+      headers: { "X-XSRF-TOKEN": "token" },
+      withCredentials: true,
+    });
+  });
+
+  test("should logout doctor user", async () => {
+    const mockData = { mockData: "mock data" };
+    axios.mockResolvedValue({ data: mockData });
+    ManagementApi.setToken("token");
+    const result = await ManagementApi.logout();
+
+    expect(result).toEqual(mockData);
+    expect(axios).toHaveBeenCalledTimes(1);
+    expect(axios).toHaveBeenCalledWith({
+      method: "post",
+      url: "/api/auth/logout",
+      data: {},
+      headers: { "X-XSRF-TOKEN": "token" },
+      withCredentials: true,
+    });
+    expect(ManagementApi.token).toBe(null);
+  });
+});
---
+++ client/src/managementApi.js
@@ -0,0 +1,238 @@
+import axios from "axios";
+
+const BASE_URL = "/api";
+
+/**
+ * Management API class to make calls to the backend API.
+ */
+class ManagementApi {
+  static token;
+
+  // API initialization
+
+  /**
+   * Set the static token property for the API class to
+   * be used in all API calls.
+   *
+   * The XSRF-TOKEN cookie should be passed as the token.
+   * @param {String} token
+   */
+  static setToken(token) {
+    this.token = token;
+  }
+
+  // Request method
+
+  /**
+   * Request method to make a call to the backend API.
+   *
+   * This method should be called by other methods in the class only.
+   *
+   * Use async function and await this method to get the response data.
+   * @param {String} endpoint
+   * @param {String} method - default is "get"
+   * @param {{}} data {RequestData} - default is empty {}
+   * @returns {Promise<ResponseDataObject>} {ResponseData}
+   */
+  static async request(endpoint, method = "get", data = {}) {
+    console.debug("API Call:", endpoint, data, method);
+    console.log(this.token);
+    const url = `${BASE_URL}${endpoint}`;
+    const headers = this.token ? { "X-XSRF-TOKEN": this.token } : {};
+    console.log(headers);
+    try {
+      return (
+        await axios({ url, method, data, headers, withCredentials: true })
+      ).data;
+    } catch (err) {
+      console.error("API Error:", err.response);
+      let message = err.response.data.error.message;
+      throw Array.isArray(message) ? message : [message];
+    }
+  }
+
+  // Patient methods
+
+  /**
+   * Create a new patient in the database and return the created patient.
+   *
+   * Use async function and await this method to get the response data.
+   * @param {{
+   *    firstName: String,
+   *    lastName: String,
+   *    dob: String,
+   *    gender: String,
+   *    email: String,
+   *    phone: String,
+   *    address: String,
+   *    city: String,
+   *    state: String,
+   *    zip: String,
+   * }} patientData
+   * @returns {Promise<PatientData>} {PatientData}
+   */
+  static async createPatient(patientData) {
+    return this.request("/patients", "post", patientData);
+  }
+
+  /**
+   * Get a patient by ID.
+   *
+   * Use async function and await this method to get the response data.
+   * @param {String} patientId
+   * @returns {Promise<PatientData>} {PatientData}
+   */
+  static async getPatient(patientId) {
+    return this.request(`/patients/${patientId}`);
+  }
+
+  /**
+   * Get a page of patients from the database.
+   *
+   * Can include an optional filter query to filter the patients.
+   *
+   * Use async function and await this method to get the response data.
+   * @param {Number} page
+   * @param {Number} limit
+   * @param {String} filter
+   * @param {String} filterValue
+   * @returns {Promise<PatientData[]>} {PatientData[]}
+   */
+  static async getPagePatients(
+    page = 1,
+    limit = 10,
+    filter = null,
+    filterValue = null
+  ) {
+    return this.request(
+      `/patients?page=${page}&limit=${limit}` +
+        (filter ? `&filter=${filter}&filterValue=${filterValue}` : "")
+    );
+  }
+
+  /**
+   * Update a patient by ID. Returns the updated patient.
+   *
+   * Use async function and await this method to get the response data.
+   * @param {{
+   *    id: String,
+   *    firstName: String,
+   *    lastName: String,
+   *    dob: String,
+   *    gender: String,
+   *    email: String,
+   *    phone: String,
+   *    address: String,
+   *    city: String,
+   *    state: String,
+   *    zip: String,
+   * }} patientData
+   * @returns {Promise<PatientData>} {PatientData}
+   */
+  static async updatePatient(patientData) {
+    return this.request(`/patients`, "put", patientData);
+  }
+
+  /**
+   * Delete a patient by ID. Returns a success message.
+   *
+   * Use async function and await this method to get the response data.
+   * @param {String} patientId
+   * @returns {Promise<{ message: String }>} { message: String }
+   */
+  static async deletePatient(patientId) {
+    return this.request(`/patients/${patientId}`, "delete");
+  }
+
+  // Prescription methods
+
+  /**
+   * Create a new prescription in the database and return the created prescription.
+   *
+   * Use async function and await this method to get the response data.
+   * @param {{
+   *    patientId: String,
+   *    medicationCode: String,
+   *    dosage: String,
+   *    refills: Number,
+   *    instructions: String,
+   *    doctorId: String,
+   * }} prescriptionData
+   * @returns {Promise<PrescriptionData>} {PrescriptionData}
+   */
+  static async createPrescription(prescriptionData) {
+    return this.request("/prescriptions", "post", prescriptionData);
+  }
+
+  /**
+   * Cancel a prescription by ID. Returns the canceled prescription.
+   *
+   * Use async function and await this method to get the response data.
+   * @param {Number} prescriptionId
+   * @returns {Promise<PrescriptionData>} {PrescriptionData}
+   */
+  static async cancelPrescription(prescriptionId) {
+    return this.request(`/prescriptions/${prescriptionId}/cancel`, "patch");
+  }
+
+  /**
+   * Get a page of prescriptions from the database.
+   *
+   * Can include an optional filter query to filter the prescriptions.
+   *
+   * Use async function and await this method to get the response data.
+   * @param {Number} page
+   * @param {Number} limit
+   * @param {String} filter
+   * @param {String} filterValue
+   * @returns {Promise<PrescriptionData[]>} {PrescriptionData[]}
+   */
+  static async getPagePrescriptions(
+    page = 1,
+    limit = 10,
+    filter = null,
+    filterValue = null
+  ) {
+    return this.request(
+      `/prescriptions?page=${page}&limit=${limit}` +
+        (filter ? `&filter=${filter}&filterValue=${filterValue}` : "")
+    );
+  }
+
+  /**
+   * Get a prescription by ID.
+   *
+   * Use async function and await this method to get the response data.
+   * @param {Number} prescriptionId
+   * @returns {Promise<PrescriptionData>} {PrescriptionData}
+   */
+  static async getPrescription(prescriptionId) {
+    return this.request(`/prescriptions/${prescriptionId}`);
+  }
+
+  // Authentication methods
+
+  /**
+   * Send token to the backend to authenticate the doctor user. Returns the doctor user data.
+   *
+   * If the doctor user does not exist in the database, a new doctor user is created.
+   *
+   * Use async function and await this method to get the response data.
+   * @returns {Promise<DoctorUserData>} {DoctorUserData}
+   */
+  static async authenticateDoctorUser() {
+    return this.request("/auth", "post");
+  }
+
+  /**
+   * Logout the doctor user by removing the token from the backend.
+   *
+   * Returns the logout url and idToken for okta logout.
+   */
+  static async logout() {
+    const response = await this.request("/auth/logout", "post");
+    this.token = null;
+    return response;
+  }
+}
+export default ManagementApi;
