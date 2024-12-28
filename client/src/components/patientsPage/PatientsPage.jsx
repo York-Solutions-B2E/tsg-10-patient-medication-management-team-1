f37057921aa452ee9f4c5e4f319cb714af199154
jhortizu01 <jhortizuela21@gmail.com>
2024-12-28 09:35:08 -0700

    display grid

--- client/src/components/patientsPage/PatientsPage.jsx
+++ client/src/components/patientsPage/PatientsPage.jsx
@@ -1,6 +1,6 @@
 import {useEffect, useState} from "react";
 import axios from "axios";
-
+import {DataGrid} from "@mui/x-data-grid";
 const PatientsPage = () => {
   const [patients, setPatients] = useState([]);
   const [loading, setLoading] = useState(true);
@@ -21,9 +21,31 @@ const PatientsPage = () => {
     fetchPatients();
   }, [])
 
+  const columns = [
+    { field: 'id', headerName: 'ID', width: 90 },
+    { field: 'firstName', headerName: 'First Name', width: 150 },
+    { field: 'lastName', headerName: 'Last Name', width: 150 },
+    { field: 'Dob', headerName: 'Date of Birth', width: 180 },
+    { field: 'Address', headerName: 'Address', width: 180 },
+    { field: 'Prescriptions', headerName: 'Prescriptions', width: 150 },
+  ]
+
+  const rows = patients.map((patient) => ({
+    id: patient.Id,
+    firstName: patient.firstName,
+    lastName: patient.lastName,
+    Dob: patient.Dob,
+    Address: `${patient.Address.Street_one} ${patient.Address.Street_two ? `, ${patient.Address.Street_two}` : ''}, ${patient.Address.City}, ${patient.Address.State} ${patient.Address.Zip}`,
+    Prescriptions: patient.Prescriptions,
+  }));
 
   return (
-      <div>Test</div>
+      <div>
+        <div>Hello</div>
+        <div style={{height: 400, width: '100%'}}>
+          <DataGrid rows={rows} columns={columns} pageSize={5}/>
+        </div>
+      </div>
   )
 };
 
