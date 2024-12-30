1c501d0412ca7f7db8249c7a2f0241eeeec98a1c
Ashton <ashtonbalder@gmail.com>
2024-12-30 13:56:31 -0600

    initialize navbar

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import '../styles/Navbar.scss';
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      {/* Home Link */}
+      <div className="nav-left">
+        <div className="nav-item">Home</div>
+      </div>
+
+      {/* Title in the Center */}
+      <div className="nav-center">
+        Medical Prescription Manager
+      </div>
+
+      {/* Links on the Right */}
+      <div className="nav-right">
+        <div className="nav-item">Patient Management</div>
+        <div className="nav-item">Prescription Management</div>
+        <div className="nav-item">New Prescription</div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
