095e7824f4cf877e9b24ccb9e7ded27bb0942363
Casey Wittrock <CwittrockC@yorksolutions.com>
2024-12-30 15:36:06 -0600

    navbar functionality

--- client/src/components/common/Navbar.jsx
+++ client/src/components/common/Navbar.jsx
@@ -1,22 +1,66 @@
-import "../styles/Navbar.scss";
+import "../../styles/Navbar.scss";
 
-const Navbar = ({ isLoggedIn }) => {
+import { Button } from "@mui/material";
+
+import { useNavigate, useLocation } from "react-router-dom";
+
+const Navbar = ({ isLoggedIn, logout }) => {
+  const navigate = useNavigate();
+  const { pathname } = useLocation();
+
   return (
     <nav className="navbar">
       {/* Home Link */}
       <div className="nav-left">
-        <div className="nav-item">Home</div>
+        <Button onClick={() => navigate("/patients")} className="nav-item">
+          Home
+        </Button>
       </div>
 
       {/* Title in the Center */}
       <div className="nav-center">Medical Prescription Manager</div>
 
       {/* Links on the Right */}
-      <div className="nav-right">
-        <div className="nav-item">Patient Management</div>
-        <div className="nav-item">Prescription Management</div>
-        <div className="nav-item">New Prescription</div>
-      </div>
+      {isLoggedIn && (
+        <div className="nav-right">
+          <Button
+            onClick={() => navigate("/patients")}
+            className={
+              `nav-item` + (pathname.includes("/patients") ? " active" : "")
+            }
+          >
+            Patient Management
+          </Button>
+          <Button
+            onClick={() => navigate("/prescriptions")}
+            // variant={pathname.includes("/prescriptions") && !pathname.includes("/create") ? "outlined" : "outlined"}
+            className={
+              `nav-item` +
+              (pathname.includes("/prescriptions") &&
+              !pathname.includes("/create")
+                ? " active"
+                : "")
+            }
+            // className="nav-item"
+          >
+            Prescription Management
+          </Button>
+          <Button
+            onClick={() => navigate("/prescriptions/create")}
+            // variant={pathname.includes("/prescriptions/create") ? "outlined" : "outlined"}
+            className={
+              `nav-item` +
+              (pathname.includes("/prescriptions/create") ? " active" : "")
+            }
+            // className="nav-item"
+          >
+            New Prescription
+          </Button>
+          <Button onClick={logout} className="nav-item logout">
+            Logout
+          </Button>
+        </div>
+      )}
     </nav>
   );
 };
