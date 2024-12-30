7b96e2e4f0ee97cd8ddb155618d97a0ba5557761
Casey Wittrock <CwittrockC@yorksolutions.com>
2024-12-30 15:48:48 -0600

    add missing AppProvider import and implimentation on main.jsx, fix missing @mui/material package in dependencies

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -10,10 +10,14 @@ import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
 
+import { AppProvider } from "./context/AppContext.jsx";
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <AppProvider>
+        <App />
+      </AppProvider>
     </BrowserRouter>
   </StrictMode>
 );
