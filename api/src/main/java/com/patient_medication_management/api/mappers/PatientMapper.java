d0d3af52dc579a07f0fc1dbfeb4c53c9cd89586a
jhortizu01 <jhortizuela21@gmail.com>
2025-01-02 07:04:46 -0700

    update to add patient prescriptionCount

--- api/src/main/java/com/patient_medication_management/api/mappers/PatientMapper.java
+++ api/src/main/java/com/patient_medication_management/api/mappers/PatientMapper.java
@@ -4,18 +4,25 @@ import com.patient_medication_management.api.dto.responses.PatientDTO;
 import com.patient_medication_management.api.patient.Patient;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
 
 @Mapper(componentModel = "spring")
 public interface PatientMapper {
 
-    @Mapping(target = "address", source = "address")
-    @Mapping(target = "prescriptionCount", expression = "java(patient.getPrescriptions() != null ? patient.getPrescriptions().size() : 0)")
+    @Mappings({
+            @Mapping(target = "street1", source = "address.street1"),
+            @Mapping(target = "street2", source = "address.street2"),
+            @Mapping(target = "city", source = "address.city"),
+            @Mapping(target = "state", source = "address.state"),
+            @Mapping(target = "zipCode", source = "address.zipCode"),
+            @Mapping(target = "prescriptionCount", expression = "java(getPrescriptionCount(patient))")
+    })
     public abstract PatientDTO mapToDTO(Patient patient);
 
     @Mapping(target = "address", ignore = true)
-    @Mapping(target = "prescriptions", ignore = true)
-    @Mapping(target = "pharmacy", ignore = true)
-    @Mapping(target = "createdAt", ignore = true)
-    @Mapping(target = "updatedAt", ignore = true)
     public abstract Patient mapToEntity(PatientDTO patientDTO);
+
+    default long getPrescriptionCount(Patient patient) {
+        return patient.getPrescriptions() != null ? patient.getPrescriptions().size() : 0;
+    }
 }
