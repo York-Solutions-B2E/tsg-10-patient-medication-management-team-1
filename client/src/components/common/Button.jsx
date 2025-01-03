4a3fd70edfc564f2cb9e49b2206c3d6c37d720ae
Casey Wittrock <CwittrockC@yorksolutions.com>
2025-01-03 12:36:21 -0600

    finish button and tests

--- client/src/components/common/Button.jsx
+++ client/src/components/common/Button.jsx
@@ -1,39 +1,56 @@
-import { Tooltip, CircularProgress } from "@mui/material"
+import { Tooltip, CircularProgress } from "@mui/material";
 
-const Button = ({type, action, selected, loading, disabled, text, icon, tooltipText}) => {
+const Button = ({
+  type,
+  action,
+  selected,
+  loading,
+  disabled,
+  text,
+  icon,
+  tooltipText,
+}) => {
   return (
-  {tooltipText ? (
-    <Tooltip title={tooltipText}>
-      <button
-        onClick={action}
-        disabled={disabled || loading}
-        className={`button ${type} ${selected ? "selected" : ""}`}
-      >
-        {loading ? (
-          <div className="loader"></div>)
-        ) : (
-          <>
-            {icon && <span className="material-icons icon">{icon}</span>}
-            { loading ? <CircularProgress size={20} /> : <span className="button-text">{text}</span> }
-          </>
-        )}
-      </button>
-    </Tooltip>
-  ) : (
-    <button
-      onClick={action}
-      disabled={disabled}
-      className={`button ${type} ${selected ? "selected" : ""}`}
-    >
-      {loading ? (
-        <div className="loader"></div>
+    <>
+      {tooltipText ? (
+        <Tooltip title={tooltipText}>
+          <button
+            onClick={action}
+            disabled={disabled || loading}
+            className={`button ${type} ${selected ? "selected" : ""}`}
+          >
+            <span>
+              {loading ? (
+                <CircularProgress size={20} />
+              ) : (
+                <>
+                  {icon && <span>{icon}</span>}
+                  {text && <span>{text}</span>}
+                </>
+              )}
+            </span>
+          </button>
+        </Tooltip>
       ) : (
-        <>
-          {icon && <span className="material-icons">{icon}</span>}
-          <span>{text}</span>
-        </>
+        <button
+          onClick={action}
+          disabled={disabled || loading}
+          className={`button ${type} ${selected ? "selected" : ""}`}
+        >
+          <span>
+            {loading ? (
+              <CircularProgress size={20} />
+            ) : (
+              <>
+                {icon && <span>{icon}</span>}
+                {text && <span>{text}</span>}
+              </>
+            )}
+          </span>
+        </button>
       )}
-    </button>
-  )})}
+    </>
+  );
+};
 
-export default Button
+export default Button;
