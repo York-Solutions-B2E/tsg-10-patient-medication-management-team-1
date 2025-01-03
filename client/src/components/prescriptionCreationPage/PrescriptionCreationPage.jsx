bd49ed25292b58d57345a24c0b06d8e38fd82c25
Casey Wittrock <CwittrockC@yorksolutions.com>
2025-01-03 17:17:55 -0600

    fixed unwanted track

--- client/src/components/prescriptionCreationPage/PrescriptionCreationPage.jsx
+++ client/src/components/prescriptionCreationPage/PrescriptionCreationPage.jsx
@@ -1,27 +1,4 @@
-import { useState, useEffect } from "react";
-import { useAppContext } from "../../context/AppContext";
-
 const PrescriptionCreationPage = () => {
-  const [prescriptions, setPrescriptions] = useState([]);
-  const [page, setPage] = useState(1);
-  const [totalPages, setTotalPages] = useState(0);
-  const [limit, setLimit] = useState(10);
-  const [filter, setFilter] = useState(null);
-  const [filterValue, setFilterValue] = useState(null);
-
-  const { handleGetPrescriptions, isLoading } = useAppContext();
-
-  useEffect(() => {
-    handleGetPrescriptions(page, limit, filter, filterValue).then(
-      (response) => {
-        setPrescriptions(response.prescriptions);
-        setTotalPages(response.totalPages);
-      }
-      if ()
-    );
-  }
-
-
   return <div>PrescriptionCreationPage</div>;
 };
 
