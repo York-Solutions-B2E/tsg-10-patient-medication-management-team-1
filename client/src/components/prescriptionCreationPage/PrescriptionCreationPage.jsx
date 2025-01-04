9eaa96a8fe5122053c89377a16752d4e34dbe5fc
Casey Wittrock <CwittrockC@yorksolutions.com>
2025-01-04 01:27:36 -0600

    initialize prescription creation page, update api and context for get methods of pharmacies and medications

--- client/src/components/prescriptionCreationPage/PrescriptionCreationPage.jsx
+++ client/src/components/prescriptionCreationPage/PrescriptionCreationPage.jsx
@@ -1,4 +1,50 @@
+import { useState, useEffect } from "react";
+import { useSearchParams } from "react-router-dom";
+import { useAppContext } from "../../context/AppContext";
+
 const PrescriptionCreationPage = () => {
+  const [searchParams] = useSearchParams();
+  const [pharmacies, setPharmacies] = useState([]);
+  const [medications, setMedications] = useState([]);
+
+  const { handleGetPharmacies, handleGetMedications, isLoading, doctorUser } =
+    useAppContext();
+
+  const formFields = [
+    { name: "doctorId", hidden: true, defaultValue: doctorUser.id },
+    {
+      name: "patientId",
+      label: "Patient ID",
+      type: "text",
+      defaultValue: searchParams.get("patientId") || "",
+    },
+    { name: "pharmacyId", label: "Pharmacy ID", type: "text" },
+    {
+      name: "medicationCode",
+      label: "Medication",
+      type: "select",
+      options: medications,
+      autocomplete: true,
+    },
+    { name: "quantity", label: "Quantity", type: "number" },
+    { name: "dosage", label: "Dosage", type: "text" },
+    { name: "instructions", label: "Instructions", type: "text" },
+  ];
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const { pharmacies, medications } = await Promise.all([
+        handleGetPharmacies(),
+        handleGetMedications(),
+      ]);
+      setPharmacies(pharmacies);
+      setMedications(medications);
+    };
+    if (pharmacies.length === 0 && medications.length === 0) {
+      fetchData();
+    }
+  }, [pharmacies, medications]);
+
   return <div>PrescriptionCreationPage</div>;
 };
 
