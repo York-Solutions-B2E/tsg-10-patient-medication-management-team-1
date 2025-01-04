6bc4371110acb978c9bdde9ab1073d5073201df1
Casey Wittrock <CwittrockC@yorksolutions.com>
2025-01-04 01:59:22 -0600

    add formFields and submit function. now waiting for form and modal component to be merged

--- client/src/components/prescriptionCreationPage/PrescriptionCreationPage.jsx
+++ client/src/components/prescriptionCreationPage/PrescriptionCreationPage.jsx
@@ -1,14 +1,22 @@
 import { useState, useEffect } from "react";
 import { useSearchParams } from "react-router-dom";
 import { useAppContext } from "../../context/AppContext";
+import { useNavigate } from "react-router-dom";
 
 const PrescriptionCreationPage = () => {
   const [searchParams] = useSearchParams();
   const [pharmacies, setPharmacies] = useState([]);
   const [medications, setMedications] = useState([]);
 
-  const { handleGetPharmacies, handleGetMedications, isLoading, doctorUser } =
-    useAppContext();
+  const navigate = useNavigate();
+
+  const {
+    handleGetPharmacies,
+    handleGetMedications,
+    handleCreatePrescription,
+    isLoading,
+    doctorUser,
+  } = useAppContext();
 
   const formFields = [
     { name: "doctorId", hidden: true, defaultValue: doctorUser.id },
@@ -16,20 +24,41 @@ const PrescriptionCreationPage = () => {
       name: "patientId",
       label: "Patient ID",
       type: "text",
+      required: true,
+      validation: "patientId",
       defaultValue: searchParams.get("patientId") || "",
     },
-    { name: "pharmacyId", label: "Pharmacy ID", type: "text" },
+    {
+      name: "pharmacyId",
+      label: "Pharmacy",
+      type: "select",
+      options: pharmacies,
+      required: true,
+    },
     {
       name: "medicationCode",
       label: "Medication",
       type: "select",
       options: medications,
       autocomplete: true,
+      required: true,
     },
-    { name: "quantity", label: "Quantity", type: "number" },
-    { name: "dosage", label: "Dosage", type: "text" },
-    { name: "instructions", label: "Instructions", type: "text" },
+    { name: "quantity", label: "Quantity", type: "number", required: true },
+    { name: "dosage", label: "Dosage", type: "text", required: true },
+    {
+      name: "instructions",
+      label: "Instructions",
+      type: "text",
+      required: true,
+    },
   ];
+
+  const handleSubmit = async (values) => {
+    if (await handleCreatePrescription(values)) {
+      // Redirect to the prescriptions page
+      navigate("/prescriptions");
+    }
+  };
 
   useEffect(() => {
     const fetchData = async () => {
