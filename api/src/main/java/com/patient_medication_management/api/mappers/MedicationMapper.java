b38500ad86a92554a2df4c44092e04968629d52b
Casey Wittrock <CwittrockC@yorksolutions.com>
2025-01-07 10:46:44 -0600

    change medicineName and code to medicationName and code

--- api/src/main/java/com/patient_medication_management/api/mappers/MedicationMapper.java
+++ api/src/main/java/com/patient_medication_management/api/mappers/MedicationMapper.java
@@ -11,18 +11,16 @@ import java.util.List;
 @Mapper(componentModel = "spring")
 public interface MedicationMapper {
 
-    @Mapping(target = "medicineName", source = "medicationName")
-    @Mapping(target = "medicineCode", source = "medicationCode")
+
+
     MedicationDTO mapToDTO(Medication medication);
 
-    @Mapping(target = "medicationName", source = "medicineName")
-    @Mapping(target = "medicationCode", source = "medicineCode")
+
     @Mapping(target = "prescriptions", ignore = true)
     @Mapping(target = "createdAt", ignore = true)
     @Mapping(target = "updatedAt", ignore = true)
     Medication mapToEntity(MedicationDTO medicationDTO);
 
-    @Mapping(target = "medicineName", source = "medicationName")
-    @Mapping(target = "medicineCode", source = "medicationCode")
+    
     List<MedicationDTO> mapToDTOs(List<Medication> medications);
 }
